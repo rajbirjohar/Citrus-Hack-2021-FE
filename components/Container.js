b43ff016f87ade696bc1e5f20e0084956e0345ab
r-jo <rdrsjck@gmail.com>
2021-03-23 08:14:38 -0700

    Added meta

--- components/Container.js
+++ components/Container.js
@@ -1,9 +1,20 @@
+import Head from "next/head";
 import Nav from "./Nav";
 import Footer from "./Footer";
+
+export const siteTitle = "Citrus Hack | FE";
 
 export default function Container({ children, main }) {
   return (
     <>
+      <Head>
+        <meta
+          name="description"
+          content="Citrus Hack reimagined."
+        />
+
+        <meta name="og:title" content={siteTitle} />
+      </Head>
       <Nav />
       <div className="flex flex-col justify-center items-center px-6">
         {children}
